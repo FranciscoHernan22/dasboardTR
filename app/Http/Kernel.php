82f01b86e7034d215e1fa2326f73152a73a4ede8
francisco <francisco>
2026-08-18 10:10:24 -0700

    panelAdministrador

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,5 +1,7 @@
 <?php
 
+ 
+
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
@@ -63,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin.web' => \App\Http\Middleware\EnsureIsAdminWeb::class,
     ];
 }
